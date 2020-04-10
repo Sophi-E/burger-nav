e64503cb40075d93e4e71108f8b550ffd713fd53
Sophia <sophierithous@gmail.com>
2020-04-10 16:19:37 +0100

    click function

---
+++ src/index.js
@@ -0,0 +1,10 @@
+// var burger = document.getElementById('burger');
+
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
